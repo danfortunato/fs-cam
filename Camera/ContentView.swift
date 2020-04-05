587e366c5b8729650c0c400145b41b8e1fe373ce
danfortunato <dan.fortunato@gmail.com>
2020-04-05 14:15:47 -0400

    Initial Commit

---
+++ Camera/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Camera
+//
+//  Created by Dan Fortunato on 4/5/20.
+//  Copyright © 2020 Dan Fortunato. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
